13433d5eaff82ad7efae3a5158197fefdb8d1ce7
redaxmedia <info@redaxmedia.com>
2019-08-27 19:48:47 +0200

    Auto confirm for SFTP

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -14,7 +14,12 @@ module.exports = grunt =>
 			options:
 			{
 				url: process.env.FTP_URL,
-				command: 'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10; exit'
+				command:
+				[
+					'set sftp:auto-confirm yes',
+					'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10',
+					'exit'
+				]
 			}
 		}
 	};
