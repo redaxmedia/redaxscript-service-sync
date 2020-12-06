67a5a1306a525dea0ba9572fca138b5a8f01e881
redaxmedia <info@redaxmedia.com>
2020-12-07 00:56:44 +0100

    Remove command from ftpress task

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -15,7 +15,6 @@ module.exports = () =>
 				commandArray:
 				[
 					'mirror {SOURCE} {TARGET}-new-{TIMESTAMP} --reverse --delete-first --parallel=10 --use-pget-n=10',
-					'chmod 0444 {TARGET}-new-{TIMESTAMP}/config.php',
 					'mv {TARGET} {TARGET}-old-{TIMESTAMP}',
 					'mv {TARGET}-new-{TIMESTAMP} {TARGET}',
 					'rm -rf {TARGET}-old-{TIMESTAMP}',
