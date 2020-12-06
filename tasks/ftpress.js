1b55475afb57e8eec51cf3eb61045c7f851bf6f2
redaxmedia <info@redaxmedia.com>
2020-12-06 17:08:55 +0100

    Update badge in README, Remove travis.yml, Adjust ftpress task

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -8,13 +8,12 @@ module.exports = () =>
 			[
 				'build'
 			],
-			dest: 'www/htdocs/w00976cb/redaxscript-service',
+			dest: 'redaxscript-service',
 			options:
 			{
 				url: process.env.FTP_URL,
 				command:
 				[
-					'set sftp:auto-confirm yes',
 					'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10',
 					'exit'
 				]
